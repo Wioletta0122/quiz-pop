c94268259a62469e6ce6b603e4d08a41d8e34aeb
CatNipLover <blastebite@gmail.com>
2025-12-25 21:32:22 +0100

    CSP

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,38 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
+  
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: 'unsafe-inline';
+    style-src 'self' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com;
+    img-src 'self' blob: data: https://*.supabase.co https://*.hcaptcha.com;
+    font-src 'self' data:;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+    connect-src 'self' https://*.supabase.co https://*.hcaptcha.com https://hcaptcha.com;
+    frame-src 'self' https://*.hcaptcha.com https://hcaptcha.com;
+  `.replace(/\s{2,}/g, ' ').trim()
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-nonce', nonce)
+  requestHeaders.set('Content-Security-Policy', cspHeader)
+
   let response = NextResponse.next({
     request: {
-      headers: request.headers,
+      headers: requestHeaders,
     },
   })
+
+  response.headers.set('Content-Security-Policy', cspHeader)
+  response.headers.set('X-Frame-Options', 'DENY')
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -18,9 +45,16 @@ export async function middleware(request: NextRequest) {
         },
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
+          
           response = NextResponse.next({
-            request,
+            request: { headers: requestHeaders },
           })
+          
+          response.headers.set('Content-Security-Policy', cspHeader)
+          response.headers.set('X-Frame-Options', 'DENY')
+          response.headers.set('X-Content-Type-Options', 'nosniff')
+          response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
+          
           cookiesToSet.forEach(({ name, value, options }) =>
             response.cookies.set(name, value, options)
           )
